57128c1c4b99f5d6faadc916313c77aa9a72e0a0
Khadaq2001 <q764579066@gmail.com>
2024-02-06 21:38:42 +0800

    update .gitignore file

--- probelmSet/d1500/pam.cpp
+++ probelmSet/d1500/pam.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 typedef long long ll;
-int t,n,a,b;
+ll t,n,a,b;
 int main(){
     cin >> t;
-    for (int i=0; i<t; i++){
-        scanf("%lld %lld %lld", &n, &a, &b); 
+    while(t--){
         
     }
 }
